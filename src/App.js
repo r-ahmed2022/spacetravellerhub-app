c3a8659ea93740347f685bcaf5b8ed3deff9d42b
shegy28 <shegy28@gmail.com>
2023-01-31 15:32:26 +0100

    fix merge issues

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navigation';
 import { LIST_ROCKETS } from './redux/Rockets/rocket';
-<<<<<<< HEAD
-=======
 import Mission from './pages/Mission';
->>>>>>> f7e3ee9e72812c90a4e509bd2efc8e7a0c49374d
 import Profile from './pages/Profile';
 import RocketList from './pages/RocketList';
 
@@ -21,28 +18,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<RocketList />} />
-<<<<<<< HEAD
-        <Route path="profile" element={<Profile />} />
-      </Routes>
-    </BrowserRouter>
-
-  );
-}
-
-export default App;
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navigation';
-import Mission from './pages/Mission';
-import Profile from './pages/Profile';
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-=======
->>>>>>> f7e3ee9e72812c90a4e509bd2efc8e7a0c49374d
         <Route path="mission" element={<Mission />} />
         <Route path="profile" element={<Profile />} />
       </Routes>
