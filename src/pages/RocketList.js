d8a887cfb935a8f9eb105ced7a2b4fe5f6977478
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-01-31 15:46:33 +0530

    fixed rocketlist linters

--- src/pages/RocketList.js
+++ src/pages/RocketList.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Rocket from './Rocket';
 
 export default function RocketList() {
-    const { list } = useSelector((state) => state?.rocketslist);
-    const dispatch = useDispatch()
-   
-     const rocketList = list?.map((rocket) => {
-          return <Rocket key={rocket.id} rocket={rocket}/>
-    })
+  const { list } = useSelector((state) => state?.rocketslist);
+  const rocketList = list?.map((rocket) => <Rocket key={rocket.id} rocket={rocket} />);
   return (
     <div className="rocketlist">
       {rocketList}
-    
+
     </div>
-  )
+  );
 }
