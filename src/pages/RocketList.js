e6ac7bb19bb8a8a8723f1650d5d74e57b63ea7c5
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-01-31 16:07:00 +0530

    updated rockets

--- src/pages/RocketList.js
+++ src/pages/RocketList.js
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import Rocket from './Rocket';
 
 export default function RocketList() {
-  const { list } = useSelector((state) => state?.rocketslist);
+  const { list } = useSelector((state) => state?.rocket);
   const rocketList = list?.map((rocket) => <Rocket key={rocket.id} rocket={rocket} />);
   return (
     <div className="rocketlist">
