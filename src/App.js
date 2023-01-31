b9ad43f66693d6b52e523a125c5a0a6937b21b19
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-01-31 14:21:39 +0530

    updated route in App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navigation';
 import Profile from './pages/Profile';
+import RocketList from './pages/RocketList';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
+        <Route path="/" element={<RocketList />} />
         <Route path="profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
