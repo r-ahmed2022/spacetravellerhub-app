c20dcaa189f31f5c3c1e664d869ed677b445f783
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-01-31 07:58:17 +0530

    created pages folder

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navigation';
-import Mission from './pages/Mission';
-import MyProfile from './pages/MyProfile';
-import Rocket from './pages/Rocket';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Rocket />} />
-        <Route path="mission" element={<Mission />} />
-        <Route path="profile" element={<MyProfile />} />
+        <Route />
+        <Route />
+        <Route />
       </Routes>
     </BrowserRouter>
 
