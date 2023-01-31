db29a7472147cb23b5e244cb13d6a945df5faf0e
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-01-31 14:14:24 +0530

    setup basic structure for display components for Rockets avaialble

---
+++ src/pages/Rocket.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Rocket() {
+  return (
+    <div>Rocket</div>
+  );
+}
---
+++ src/pages/RocketList.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Rocket from './Rocket';
+
+export default function RocketList() {
+  const dispatch = useDispatch();
+
+  return (
+    <div>RocketList</div>
+  );
+}
