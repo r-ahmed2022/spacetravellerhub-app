dbfafe13b5f17e5ddb46f2741d9a21745a46e3ca
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-01-31 07:48:07 +0530

    updated app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navigation';
-import Mission from './pages/Mission';
 import Profile from './pages/Profile';
-import Rocket from './pages/Rocket';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Rocket />} />
-        <Route path="mission" element={<Mission />} />
         <Route path="profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
