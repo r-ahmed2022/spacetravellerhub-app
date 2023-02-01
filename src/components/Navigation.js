21cb3c60769b72efd2353a421600ed38e113bbf6
shegy28 <shegy28@gmail.com>
2023-02-01 14:58:01 +0100

    Add style to navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import img from '../assets/planet.png';
+import img from '../assets/blacklogo.png';
 
 const Navbar = () => (
   <header>
     <nav>
       <div className="logo">
         <img src={img} className="logo-img" alt="space pic" />
-        <h2>Space Traveller Hub</h2>
+        <h2>Space Traveler's Hub</h2>
       </div>
       <ul className="nav-items">
         <li>
           <NavLink className="link" to="/">Rockets</NavLink>
         </li>
         <li><NavLink className="link" to="/mission">Missions</NavLink></li>
-        <li><NavLink className="link" to="/profile">My Profile</NavLink></li>
+        <li><NavLink className="link prof" to="/profile">My Profile</NavLink></li>
       </ul>
     </nav>
   </header>
