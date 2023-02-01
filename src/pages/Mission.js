cae4c02e97b21d84c1bd039ec04938e8cd1a7730
shegy28 <shegy28@gmail.com>
2023-02-01 10:38:03 +0100

    Fix re-render isues in mission page

--- src/pages/Mission.js
+++ src/pages/Mission.js
@@ -1,19 +1,12 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { useSelector, } from 'react-redux';
 import MissionList from '../components/MissionList';
-import { LIST_MISSIONS } from '../redux/Missions/mission';
+
 
 const Mission = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(LIST_MISSIONS());
-  }, [dispatch]);
-
+  
   const {missionList} = useSelector((state) => state?.mission);
   
- 
-
   return (
     <div>
       <MissionList  missions={missionList} />
